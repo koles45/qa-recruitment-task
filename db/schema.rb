742c0f88b2788b7cd0dcbf81f3c99a07687faa92
koles45 <grzegorzborowik@hotmail.com>
2017-02-24 17:47:32 +0100

    [O] Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -54,12 +54,12 @@ ActiveRecord::Schema.define(version: 20170116103139) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                                 null: false
     t.string   "name",                                  null: false
+    t.string   "sso_id",                                null: false
+    t.boolean  "szama"
     t.integer  "birthday_month"
     t.integer  "birthday_day"
     t.text     "about"
     t.boolean  "participating",          default: true
-    t.string   "sso_id"
-    t.string   "szama"
     t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
